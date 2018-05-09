f9b09367200a9b72393427473dec442168ab79db
Acer-PC\Acer <rahayup336@gmail.com>
2018-05-09 09:48:52 +0800

    tambah file

---
+++ ayu3/aligator.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class aligator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class aligator extends Actor
+{
+    /**
+     * Act - do whatever the aligator wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        move(4);
+        if(Greenfoot.getRandomNumber(100)<10)
+        turn(Greenfoot.getRandomNumber(15)-30);
+        eatular();
+    }    
+     public void eatular()
+    {
+        if(isTouching(ular.class))
+        removeTouching(ular.class);
+    }    
+}
+
---
+++ ayu3/sand.java
@@ -0,0 +1,52 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class sand here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class sand extends World
+{
+
+    /**
+     * Constructor for objects of class sand.
+     * 
+     */
+    public sand()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        ular ular = new ular();
+        addObject(ular,52,278);
+        cherie cherie = new cherie();
+        addObject(cherie,238,325);
+        cherie cherie2 = new cherie();
+        addObject(cherie2,205,218);
+        cherie cherie3 = new cherie();
+        addObject(cherie3,280,122);
+        cherie cherie4 = new cherie();
+        addObject(cherie4,285,38);
+        cherie cherie5 = new cherie();
+        addObject(cherie5,337,345);
+        cherie cherie6 = new cherie();
+        addObject(cherie6,316,240);
+        cherie cherie7 = new cherie();
+        addObject(cherie7,370,140);
+        cherie cherie8 = new cherie();
+        addObject(cherie8,383,70);
+        aligator aligator = new aligator();
+        addObject(aligator,225,87);
+        aligator aligator2 = new aligator();
+        addObject(aligator2,438,311);
+    }
+}
