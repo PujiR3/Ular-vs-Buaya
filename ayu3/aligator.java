a38db3c16db5a369546299553d710be292079fab
Acer-PC\Acer <rahayup336@gmail.com>
2018-05-12 21:59:58 +0800

    menambah

--- ayu3/aligator.java
+++ ayu3/aligator.java
@@ -24,7 +24,6 @@ public class aligator extends Actor
     {
         if(isTouching(ular.class))
         removeTouching(ular.class);
-        getWorld().showText("You lose",300,200);
     }    
 }
 
